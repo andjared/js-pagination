a45a9237717eab3a97adf53a36bf60b63c5b2f1d
andja <andja.velimirovic@gmail.com>
2022-12-28 14:31:58 +0100

    pagination functionality

--- app.js
+++ app.js
@@ -1,35 +1,76 @@
-// access key
-// 6e021c6d1457f08717ac75ec6f99e561
-const key = "6e021c6d1457f08717ac75ec6f99e561";
+const key = "?access_key=6e021c6d1457f08717ac75ec6f99e561";
+const baseUrl = ` http://api.coinlayer.com/`;
+const listUrl = `${baseUrl}list${key}`;
 const list = document.querySelector(".list");
 
-const getList = async () => {
-  const data = await fetch(`http://api.coinlayer.com/list?access_key=${key}`);
-  const response = data.json();
-  return response;
+//pagination variables
+const itemsPerPage = 8;
+let numOfPages;
+let currentPage = 1;
+
+//data variables
+let coinList = [];
+
+const getData = async () => {
+  const response = await fetch(listUrl);
+  const data = response.json();
+  return data;
 };
 
-getList().then((res) => {
-  const data = res.crypto;
-  console.log(data);
-  for (const coins in data) {
-    //destructure data object
-    const { name, symbol, icon_url } = data[coins];
-    //create li item for each coin & append it to ul
-    const listItem = document.createElement("li");
-    list.appendChild(listItem);
-    //create elements for name, symbol and icon & append it to li items
-    const coinIcon = document.createElement("img");
-    coinIcon.setAttribute("src", icon_url);
-    coinIcon.setAttribute("alt", name);
-    listItem.appendChild(coinIcon);
+const renderData = async (currentPage = 1) => {
+  await getData().then((data) => {
+    const coins = data.crypto;
+    const dataAsArray = Object.keys(coins);
 
-    const coinName = document.createElement("h3");
-    coinName.innerHTML = name;
-    listItem.appendChild(coinName);
+    for (let key in dataAsArray) {
+      let val = dataAsArray[key];
+      coinList.push(coins[val]);
+    }
 
-    const coinSymbol = document.createElement("p");
-    coinSymbol.innerHTML = `Coin symbol: <span>${symbol}</span>`;
-    listItem.appendChild(coinSymbol);
+    //html template
+    let listItem = "";
+    coinList
+      .filter((item, index) => {
+        let start = (currentPage - 1) * itemsPerPage;
+        let end = currentPage * itemsPerPage;
+        if (index >= start && index < end) {
+          console.log(index);
+          return true;
+        }
+      })
+      .forEach((coin) => {
+        const { name, symbol, icon_url } = coin;
+        listItem += `<li>`;
+        listItem += `<img src=${icon_url} alt=${name} />`;
+        listItem += `<h3>${name}</h3>`;
+        listItem += `<p>Coin symbol: <span>${symbol}</span> </p>`;
+        listItem += `</li>`;
+      });
+    list.innerHTML = listItem;
+  });
+};
+
+function numOfPages() {
+  return Math.ceil(coinList.length / itemsPerPage);
+}
+
+const nextPage = () => {
+  if (currentPage < numOfPages()) {
+    currentPage++;
   }
-});
+  // console.log(currentPage);
+  renderData(currentPage);
+};
+
+const prevPage = () => {
+  if (currentPage > 1) {
+    currentPage--;
+  }
+  renderData(currentPage);
+};
+
+renderData();
+
+document.querySelector(".next").addEventListener("click", nextPage);
+
+document.querySelector(".prev").addEventListener("click", prevPage);
